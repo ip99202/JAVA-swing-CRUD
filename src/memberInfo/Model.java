b60e6c6a80558b96a4d8be740a4a3cf72e534592
ip99202 <ip99202@naver.com>
2020-09-05 13:42:37 +0900

    Create model

--- src/memberInfo/Model.java
+++ src/memberInfo/Model.java
@@ -1,4 +1,40 @@
 package memberInfo;
 
 public class Model {
+    String name;
+    String birth;
+    String tel;
+
+    public Model() {
+    }
+
+    public Model(String name, String birth, String tel) {
+        this.birth = birth;
+        this.name = name;
+        this.tel = tel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 }
